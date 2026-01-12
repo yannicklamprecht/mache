76b1e9f39924e1850d21a9346d84f47e69986dac
Yannick Lamprecht <yannicklamprecht@live.de>
2026-01-12 22:08:34 +0100

    patches 26.1-snapshot-2

--- versions/26.1-snapshot-2/build.gradle.kts
+++ versions/26.1-snapshot-2/build.gradle.kts
@@ -10,7 +10,7 @@ repositories {
 
 val generateReportsProperty = providers.gradleProperty("generateReports")
 mache {
-    minecraftVersion = "26.1-snapshot-1"
+    minecraftVersion = "26.1-snapshot-2"
     minecraftJarType = MinecraftJarType.SERVER
 
     val args = mutableListOf(
