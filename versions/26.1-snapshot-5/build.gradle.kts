25447f53a567dd20a4987ead3363c37cd0ba39ce
Yannick Lamprecht <yannicklamprecht@live.de>
2026-01-31 19:44:49 +0100

    update 26.1-snapshot-5

---
+++ versions/26.1-snapshot-5/build.gradle.kts
@@ -0,0 +1,48 @@
+import io.papermc.sculptor.shared.util.MinecraftJarType
+
+plugins {
+    id("io.papermc.sculptor.version") version "2.0.0-SNAPSHOT"
+}
+
+repositories {
+    maven("https://central.sonatype.com/repository/maven-snapshots/")
+}
+
+val generateReportsProperty = providers.gradleProperty("generateReports")
+mache {
+    minecraftVersion = "26.1-snapshot-5"
+    minecraftJarType = MinecraftJarType.SERVER
+
+    val args = mutableListOf(
+        "--temp-dir={tempDir}",
+        "--unpick-file={constantsFile}",
+        "--output={output}",
+        "--input={input}",
+        "--input-classpath={inputClasspath}",
+        "--hypo-parallelism=1",
+    )
+    if (generateReportsProperty.getOrElse("false").toBooleanStrict()) {
+        args.addAll(listOf(
+            "--reports-dir={reportsDir}",
+            "--all-reports",
+        ))
+    }
+
+    codebookArgs = args
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    options.compilerArgs.add("-parameters")
+}
+
+dependencies {
+    codebook("2.0.0-SNAPSHOT")
+    decompiler(vineflower("1.12.0-SNAPSHOT"))
+    constants("io.papermc.parchment.data:parchment:1.21.11+build.3")
+}
+
+dependencies {
+    compileOnly("org.jetbrains:annotations:24.0.1")
+    compileOnly("com.google.code.findbugs:jsr305:3.0.2")
+    compileOnly("org.checkerframework:checker-qual:3.49.0")
+}
