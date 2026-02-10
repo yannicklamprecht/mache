6dad66c848f0f00815f898050691e79a41c18ad5
Yannick Lamprecht <yannicklamprecht@live.de>
2026-02-10 18:12:41 +0100

    update to release 26

--- versions/26.1/build.gradle.kts
+++ versions/26.1/build.gradle.kts
@@ -10,7 +10,7 @@ repositories {
 
 val generateReportsProperty = providers.gradleProperty("generateReports")
 mache {
-    minecraftVersion = "26.1-snapshot-6"
+    minecraftVersion = "26.1"
     minecraftJarType = MinecraftJarType.SERVER
 
     val args = mutableListOf(
